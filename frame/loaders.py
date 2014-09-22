16663980388de7d62ccb7821a138bd4cd658eb5e
Alex Eftimie <alex@eftimie.ro>
2014-09-22 16:56:24 +0300

    Refactor: move template loader to its own module.

---
+++ frame/loaders.py
@@ -0,0 +1,52 @@
+from django.conf import settings
+from django.template import TemplateDoesNotExist
+from django.template.loader import BaseLoader
+from frame.middleware import get_current_request
+from frame.utils import get_forwarded_cookies
+import requests
+
+
+class Loader(BaseLoader):
+    is_usable = True
+
+    def _process_resp(self, html):
+        substitutions = [
+            ("{%", "{% templatetag openblock %}"),
+            ("%}", "{% templatetag closeblock %}"),
+            ("{{", "{% templatetag openvariable %}"),
+            ("}}", "{% templatetag closevariable %}"),
+            ("<!-- block_messages -->",
+             "{% block action_buttons %}{% endblock %}"
+             "{% block messages %}{% endblock %}"),
+            ("<!-- block_content -->",
+             "{% block zope_content %}{% endblock %}"),
+            ("<!-- block_head -->",
+             "{% block head %}{% endblock %}"),
+            ("src=\"misc_/", "src=\"/misc_/"),
+        ]
+
+        if getattr(settings, 'FRAME_EXTRA_SUBSTITUTIONS', None):
+            substitutions += settings.FRAME_EXTRA_SUBSTITUTIONS
+
+        html = html.strip()
+        for sub_a, sub_b in substitutions:
+            html = html.replace(sub_a, sub_b)
+        return html
+
+    def load_template_source(self, template_name, template_dirs=None):
+        request = get_current_request()
+
+        if (request and getattr(settings, 'FRAME_URL', None)
+            and template_name == 'frame.html'):
+
+            forwarded_cookies = get_forwarded_cookies(request)
+
+            resp = requests.get(settings.FRAME_URL, cookies=forwarded_cookies)
+            resp_json = resp.json()
+            if (resp.status_code == 200 and resp_json):
+                frame_response = self._process_resp(resp_json['frame_html'])
+                return frame_response, template_name
+
+        raise TemplateDoesNotExist
+
+    load_template_source.is_usable = True
--- frame/middleware.py
+++ frame/middleware.py
@@ -1,10 +1,10 @@
 from threading import local
+
+from frame.loaders import Loader
 import requests
 
 from frame.utils import get_current_language, get_forwarded_cookies
 from django.conf import settings
-from django.template.base import TemplateDoesNotExist
-from django.template.loader import BaseLoader
 
 
 _thread_locals = local()
@@ -59,50 +59,5 @@ class UserMiddleware(object):
             request.user_groups = []
 
 
-class Loader(BaseLoader):
-    is_usable = True
-
-    def _process_resp(self, html):
-        substitutions = [
-            ("{%", "{% templatetag openblock %}"),
-            ("%}", "{% templatetag closeblock %}"),
-            ("{{", "{% templatetag openvariable %}"),
-            ("}}", "{% templatetag closevariable %}"),
-            ("<!-- block_messages -->",
-             "{% block action_buttons %}{% endblock %}"
-             "{% block messages %}{% endblock %}"),
-            ("<!-- block_content -->",
-             "{% block zope_content %}{% endblock %}"),
-            ("<!-- block_head -->",
-             "{% block head %}{% endblock %}"),
-            ("src=\"misc_/", "src=\"/misc_/"),
-        ]
-
-        if getattr(settings, 'FRAME_EXTRA_SUBSTITUTIONS', None):
-            substitutions += settings.FRAME_EXTRA_SUBSTITUTIONS
-
-        html = html.strip()
-        for sub_a, sub_b in substitutions:
-            html = html.replace(sub_a, sub_b)
-        return html
-
-    def load_template_source(self, template_name, template_dirs=None):
-        request = get_current_request()
-
-        if (request and getattr(settings, 'FRAME_URL', None)
-            and template_name == 'frame.html'):
-
-            forwarded_cookies = get_forwarded_cookies(request)
-
-            resp = requests.get(settings.FRAME_URL, cookies=forwarded_cookies)
-            resp_json = resp.json()
-            if (resp.status_code == 200 and resp_json):
-                frame_response = self._process_resp(resp_json['frame_html'])
-                return frame_response, template_name
-
-        raise TemplateDoesNotExist
-
-    load_template_source.is_usable = True
-
-
-_loader = Loader()
+# keep this for compatibility
+from frame.loaders import Loader
