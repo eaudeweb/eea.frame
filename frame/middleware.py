3ad1a5cbad92533f4c4c664de56acda5d8edb16c
Alex Eftimie <alex@eftimie.ro>
2015-02-25 12:12:28 +0200

    Bump version, fail safe when no frame is found

--- frame/middleware.py
+++ frame/middleware.py
@@ -1,11 +1,14 @@
 from threading import local
 import requests
+import logging
 
 from frame.utils import get_current_language, get_forwarded_cookies
 from django.conf import settings
 
 
 _thread_locals = local()
+
+logger = logging.getLogger('eea.frame')
 
 
 def get_current_request():
@@ -23,15 +26,25 @@ class RequestMiddleware(object):
 
 
 class UserMiddleware(object):
-    def process_request(self, request):
+    def _fetch_data(self):
         request = get_current_request()
-        if getattr(settings, 'FRAME_URL', None):
-            forwarded_cookies = get_forwarded_cookies(request)
-            verify = getattr(settings, 'FRAME_VERIFY_SSL', None)
+        forwarded_cookies = get_forwarded_cookies(request)
+        verify = getattr(settings, 'FRAME_VERIFY_SSL', None)
+        try:
             resp = requests.get(settings.FRAME_URL, cookies=forwarded_cookies,
                                 verify=verify)
-            resp_json = resp.json()
-            if (resp.status_code == 200 and resp_json):
+            data = resp.json()
+        except Exception as e:
+            logging.exception(e)
+            data = {}
+        else:
+            data.setdefault('frame_html', '')
+        return data
+
+    def process_request(self, request):
+        if getattr(settings, 'FRAME_URL', None):
+            resp_json = self._fetch_data()
+            if resp_json:
                 request.user_id = resp_json['user_id']
                 request.user_roles = resp_json['user_roles']
                 request.user_groups = resp_json['groups']
