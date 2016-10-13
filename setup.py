eb192906f31ceef9d49863112907d5e039eb2c89
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2016-10-13 15:22:17 +0300

    Add migrations & clean up project structure.

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
+import os
 from setuptools import setup
+
 from frame import __version__ as VERSION
 
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name='eea.frame',
     version=VERSION,
-    description='EEA Frame',
-    packages=['frame'],
+    packages=[
+        'frame',
+        'frame.migrations',
+    ],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Django>=1.6',
+        'Django>=1.9',
         'requests>=2.2',
     ],
+    description='Django integration middleware for EEA Zope websites',
+    long_description=README,
 )
